e9232d785528c18b086743d6117dec777fec7e9c
nicolas-gourdon <nic7_@hotmail.fr>
2016-05-08 20:10:17 +0200

    Add new script GridManager

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridManager : MonoBehaviour {
+
+	[HideInInspector]
+	public GameObject[,] _Dungeon;
+	int DungeonLength = 20;
+	int DungeonWidth = 20;
+
+	// Use this for initialization
+	void Start () 
+	{
+		_Dungeon = new GameObject[DungeonLength, DungeonWidth];
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
