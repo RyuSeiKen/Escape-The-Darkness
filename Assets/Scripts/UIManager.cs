c792294d1477a7f0175cda6106156b671eb6824e
James Petit <jp.james.petit@gmail.com>
2016-05-10 00:48:46 +0200

    Interface dans Donjon.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,21 +5,35 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour 
 {
 	public Text time;
-	int a = 0;
-	int b = 0;
+	public Text level;
+	public Image key;
+	int minutes = 0;
+	int seconds = 0;
+	int levelNumber = 1;
+	bool gotKey = false;
 
 	void Update () 
 	{
-		a = (int)Time.time;
-		b = a / 60;
+		seconds = (int)Time.time;
+		minutes = seconds / 60;
 
-		if(a%60 < 10)
+		if(seconds%60 < 10)
 		{
-			time.text = b + ":0" + a%60;
+			time.text = minutes + ":0" + seconds%60;
 		}
 		else
 		{
-			time.text = b + ":" + a%60;
+			time.text = minutes + ":" + seconds%60;
+		}
+		level.text = "Level " + levelNumber;
+
+		if(gotKey == true)
+		{
+			key.GetComponent<Image>().enabled = true;
+		}
+		else
+		{
+			key.GetComponent<Image>().enabled = false;
 		}
 	}
 }
