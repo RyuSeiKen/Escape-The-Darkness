6f499972aefa5dbeafef7fd1742fab4ac1bc5865
nicolas-gourdon <nic7_@hotmail.fr>
2016-05-09 03:36:31 +0200

    Victoireeeeeee

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -6,17 +6,27 @@ public class DungeonGenerator : MonoBehaviour {
 
 	public GameObject _StartRoom;
 
+
 	GameObject GameManager;
 	GridManager Grid;
+	RoomManager RoomLists;
+	GameObject ChosenRoom;
+
+
+	int NewRoomXpos;
+	int NewRoomYpos;
 
 	List<GameObject> NewRooms = new List<GameObject>();
 	List<GameObject> InstantiateRooms = new List<GameObject>();
+	List<GameObject> Temp = new List<GameObject> ();
 
 	// Use this for initialization
 	void Start () {
-		
+
+
 		GameManager = GameObject.FindGameObjectWithTag ("Manager");
 		Grid = GameManager.GetComponent<GridManager> ();
+		RoomLists = GameManager.GetComponent<RoomManager> ();
 
 		GameObject NewRoom = Instantiate (_StartRoom) as GameObject;
 
@@ -77,14 +87,14 @@ public class DungeonGenerator : MonoBehaviour {
 		Room CurrentRoomProperties = CurrentRoom.GetComponent<Room> ();
 		int CurrentRoomX = CurrentRoomProperties.Xpos;
 		int CurrentRoomY = CurrentRoomProperties.Ypos;
+		CheckRooms (side, CurrentRoomProperties.Xpos, CurrentRoomProperties.Ypos);
 
-		
 		switch(side) 
 		{
 		case 1:
 			if (Grid._Dungeon [CurrentRoomX, CurrentRoomY + 1] == null) 
 			{
-				GameObject NextRoomUp = Instantiate (_StartRoom) as GameObject;
+				GameObject NextRoomUp = Instantiate (ChosenRoom) as GameObject;
 			
 				InstantiateRooms.Add (NextRoomUp);
 
@@ -103,7 +113,7 @@ public class DungeonGenerator : MonoBehaviour {
 
 		case 2:
 			if (Grid._Dungeon [CurrentRoomX, CurrentRoomY - 1] == null) {
-				GameObject NextRoomDown = Instantiate (_StartRoom) as GameObject;
+				GameObject NextRoomDown = Instantiate (ChosenRoom) as GameObject;
 
 				InstantiateRooms.Add (NextRoomDown);
 
@@ -121,7 +131,7 @@ public class DungeonGenerator : MonoBehaviour {
 
 		case 3:
 			if (Grid._Dungeon [CurrentRoomX - 1, CurrentRoomY] == null) {				
-				GameObject NextRoomLeft = Instantiate (_StartRoom) as GameObject;
+				GameObject NextRoomLeft = Instantiate (ChosenRoom) as GameObject;
 
 				InstantiateRooms.Add (NextRoomLeft);
 
@@ -139,7 +149,7 @@ public class DungeonGenerator : MonoBehaviour {
 
 		case 4:
 			if (Grid._Dungeon [CurrentRoomX + 1, CurrentRoomY] == null) {
-				GameObject NextRoomRight = Instantiate (_StartRoom) as GameObject;
+				GameObject NextRoomRight = Instantiate (ChosenRoom) as GameObject;
 
 				InstantiateRooms.Add (NextRoomRight);
 				Grid._Dungeon [CurrentRoomX + 1, CurrentRoomY] = NextRoomRight;
@@ -167,12 +177,278 @@ public class DungeonGenerator : MonoBehaviour {
 		InstantiateRooms.Clear ();
 	}
 
+	void CheckRooms(int side, int RoomCheckedXpos, int RoomCheckedYpos) 
+	{	
+		
+		int _up;
+		int _down;
+		int _left;
+		int _right;
+
+
+
+		switch(side)
+		{
+		case 1: 
+			NewRoomXpos = RoomCheckedXpos;
+			NewRoomYpos = RoomCheckedYpos + 1;
+
+			break;
+
+		case 2:
+			NewRoomXpos = RoomCheckedXpos;
+			NewRoomYpos = RoomCheckedYpos - 1;
+
+			break;
+
+		case 3:
+			NewRoomXpos = RoomCheckedXpos - 1;
+			NewRoomYpos = RoomCheckedYpos;
+
+			break;
+
+		case 4:
+			NewRoomXpos = RoomCheckedXpos + 1;
+			NewRoomYpos = RoomCheckedYpos;
+
+			break;
+
+
+		}
+
+		if (Grid._Dungeon [NewRoomXpos, NewRoomYpos + 1] != null) {
+
+			Room UpRoomProperties = Grid._Dungeon [NewRoomXpos, NewRoomYpos + 1].GetComponent<Room> ();
+			if (UpRoomProperties._down == true) {
+				
+				_up = 1;
+			} else 
+			{
+				_up = 0;
+			}
+
+
+		} else { _up = 2;}
+
+
+
+		if (Grid._Dungeon [NewRoomXpos, NewRoomYpos - 1] != null) {
+
+			Room DownRoomProperties = Grid._Dungeon [NewRoomXpos, NewRoomYpos - 1].GetComponent<Room> ();
+			if (DownRoomProperties._up == true) {
+				
+				_down = 1;
+
+			} else 
+			{
+				_down = 0;
+			}
+
+
+		} else { _down = 2;}
+
+
+		if (Grid._Dungeon [NewRoomXpos - 1 , NewRoomYpos] != null) {
+
+			Room LeftRoomProperties = Grid._Dungeon [NewRoomXpos - 1, NewRoomYpos].GetComponent<Room> ();
+			if (LeftRoomProperties._right == true) {
+
+				_left = 1;
+			} else 
+			{
+				_left = 0;
+			}
+
+
+		} else { _left = 2;}
+
+
+		if (Grid._Dungeon [NewRoomXpos + 1, NewRoomYpos] != null) {
+			
+			Room RightRoomProperties = Grid._Dungeon [NewRoomXpos + 1, NewRoomYpos].GetComponent<Room> ();
+
+			if (RightRoomProperties._left == true) {
+
+				_right = 1;
+			} else 
+			{
+				_right = 0;
+			}
+
+
+		} else { _right = 2;}
+
+		//Debug.Log (_up +""+ _down +""+ _left +""+ _right);
+		CheckArround (_up, _down, _left, _right);
+
+
+
+	}
+
+	void CheckArround (int up, int down, int left, int right)
+	{
+		
+		bool UpCheck = false;
+		bool DownCheck = false;
+		bool LeftCheck = false;
+		bool RightCheck = false;
+		bool AllCheck = false;
+
+
+		while (AllCheck == false) {
+
+			bool enterUp = false;
+			bool enterDown = false;
+			bool enterLeft = false;
+			bool enterRight = false;
+			
+			UpCheck = false;
+			DownCheck = false;
+			LeftCheck = false;
+			RightCheck = false;
+			
+			int randomRoom = Random.Range (0, 15);
+			ChosenRoom = RoomLists._AllRooms [randomRoom];
+
+			foreach (GameObject ChoseUp in RoomLists._upRooms) {
+			
+				if (ChosenRoom == ChoseUp) {
+
+					if (up == 0) {
+						enterUp = true;
+						UpCheck = false;
+					} else {
+						UpCheck = true;
+					}
+									
+				}
+			}
+			
+
+			if (UpCheck == false ) {
+
+				if (up == 0 && enterUp == false) {
+					UpCheck = true;
+				}
+
+				if (up == 2) {
+					UpCheck = true;
+				}
+
+			}
+			foreach (GameObject ChoseDown in RoomLists._downRooms) {
+
+				if (ChosenRoom == ChoseDown) {
+
+					if (down == 0) {
+						enterDown = true;
+						DownCheck = false;
+					} else {
+						DownCheck = true;
+					}
+										
+				}
+			}
+
+
+
+			if (DownCheck == false ) {
+
+				if (down == 0 && enterDown == false) {
+					DownCheck = true;
+				}
+
+				if (down == 2) {
+					DownCheck = true;
+				}
+
+			}
+			foreach (GameObject ChoseLeft in RoomLists._leftRooms) {
+
+				if (ChosenRoom == ChoseLeft) {
+
+					if (left == 0) {
+						enterLeft = true;
+						LeftCheck = false;
+					} else {
+						LeftCheck = true;
+					}
+		
+				}
+			}
+
+
+			if (LeftCheck == false ) {
+
+				if (left == 0 && enterLeft == false) {
+					LeftCheck = true;
+				}
+
+				if (left == 2) {
+					LeftCheck = true;
+				}
+
+			}
+			foreach (GameObject ChoseRight in RoomLists._rightRooms) {
+
+				if (ChosenRoom == ChoseRight) {
+					
+
+					if (right == 0) {
+						
+						enterRight = true;
+						RightCheck = false;
+					} else {
+						
+						RightCheck = true;
+					}
+											
+				}
+			}
+
+
+			if (RightCheck == false ) {
+				
+					if (right == 0 && enterRight == false) {
+						RightCheck = true;
+					}
+
+					if (right == 2) {
+						RightCheck = true;
+					}
+
+			}
+		
+			if (UpCheck == true && RightCheck == true && DownCheck == true && LeftCheck == true) 
+			{
+				AllCheck = true;
+			}
+
+		}
+
+		AllCheck = false;
+	}
+
+	void ShuffleList(){
+
+		for (int i = 0; i < 15; i++) 
+			
+		{
+			GameObject shuffledRoom = RoomLists._AllRooms [i];
+			RoomLists._AllRooms.RemoveAt(i);
+			RoomLists._AllRooms.Add (shuffledRoom);
+			
+		}
+
+
+	}
 
 	// Update is called once per frame
 	void Update () 
 	{
 		if (Input.GetKeyDown (KeyCode.Space)) 
 		{
+			ShuffleList ();
 			Generation ();
 		}
 	}
--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 public class RoomManager : MonoBehaviour {
 
+	public List<GameObject> _AllRooms = new List<GameObject> ();
 	public List<GameObject> _leftRooms = new List<GameObject> ();
 	public List<GameObject> _rightRooms = new List<GameObject> ();
 	public List<GameObject> _upRooms = new List<GameObject> ();
